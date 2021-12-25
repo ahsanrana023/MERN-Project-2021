93e5da93b91db13eb4b0a6fd4ed70a86e3eef350
Ahsan Rana <91425343+ahsanrana023@users.noreply.github.com>
2021-12-25 19:41:22 +0500

    aaa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,51 @@
 import React from "react";
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light custom-nav">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            TrendTravels
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Packages
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
